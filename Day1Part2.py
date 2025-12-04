e05f4716020c8f461a02fbf953d322f89edada6f
Ian Weber <iacweber@gmail.com>
2025-12-04 12:59:23 -0800

    implementation begins

--- Day1Part2.py
+++ Day1Part2.py
@@ -1,1 +1,20 @@
-cd
+dialValue = 50
+count = 0
+with open("inputday1part1.txt") as file:
+    for line in file:
+        first = line[0]
+        num = int(line[1:])
+        pos = dialValue
+        old = pos
+        if first == "R":
+            dialValue = dialValue + num
+            if dialValue >= 100:
+                rotation = dialValue // 100
+                count = count + rotation
+        else:
+            dialValue = dialValue - num
+            if dialValue <= 0:
+                rotation = abs(dialValue // 100)
+                count = count + rotation
+        dialValue = dialValue % 100
+print(count)
